8f005bbedf71e1e58df4e89e67d31b2b9a9cfc23
Nicholas Clawson <nickclaw@gmail.com>
2012-10-18 00:27:51 -0700

    mouseEvents, timeFrom, commenting, style
    Add mouse events so some debugging can be done on a computer. Added
    utility function timeFrom to reduce redundancy. Cleaner style, and lots
    of comments.

--- t2c/T2Cv2.js
+++ t2c/T2Cv2.js
@@ -1,33 +1,54 @@
-var debug;
-var tl;
-var tr;
-var bl;
-var br;
-var corners;
+// T2C algorithm;
 
-var width;
-var height;
+var debug; // paragraph to print debug info
 
+// refers to corner images
+var tl; //top left
+var tr; //top right
+var bl; //bottom left
+var br; //bottom right
+
+var corners; // array of corner images
+
+var buttons; // array of all buttons
+
+var width; // width of window
+var height; // height of window
+
+// variable for touch move to store where it came from for calculating pathL
+// keep global because it saves space...
 var lastX;
 var lastY;
-var startX;
+
+//saves initial touch location onStart
+var startX; 
 var startY;
-var startDate;
-var pathL;
-var isDown = false;
 
-var corner_size = 200;
-var max_time = 1000;
-var sensitivity = 300;
-var directness = 1.2;
+var startTime; // saves start time of touch
+var pathL; // distance travelled over entire touch sequence
+var isDown = false; // true if finger is on screen or mouse button is down
 
-var c;
+var CORNER_SIZE = 200; // diameter of corner
+var SENSITIVITY = 150; // distance from edge of corner to start scaling corner
 
-function onStart() {
+var MAX_TIME = 1000; // maximum time a swipe to the corner will register as a valid swipe
+var DIRECTNESS = 1.2; // minimum value for distance/displacement for the swipe to register
+
+// called when the document is initially loaded
+// adds necessary event listeners to document
+// gets and initializes necessary elements/variables
+// puts event listener on all buttons
+// sets size of each corner
+function onLoad() {
 	document.addEventListener("touchstart", onTouchStart, false);
 	document.addEventListener("touchmove", onTouchMove, false);
 	document.addEventListener("touchend", onTouchEnd, false);
 //	document.addEventListener("touchcancel", onTouchCancel, false);
+	
+	// NONE OF THESE ARE NEEDED FOR TOUCHSCREEN
+	document.addEventListener("mousedown", onMouseDown, false);
+	document.addEventListener("mouseup", onMouseUp, false);
+	document.addEventListener("mousemove", onMouseMove, false);
 	
 	debug = document.getElementById("coord");
 	tl = document.getElementById("tl");
@@ -35,22 +56,44 @@ function onStart() {
 	bl = document.getElementById("bl");
 	br = document.getElementById("br");
 	corners = document.getElementsByClassName("corner");
+	buttons = document.getElementsByClassName("btn");
 	
 	width = window.innerWidth;
 	height = window.innerHeight;
 	
+	for (var i = 0; i < buttons.length; i++){
+		buttons[i].onclick = clicked;
+		buttons[i].touchenter = touchEnter;
+		buttons[i].touchleave = touchLeave;
+	}
+
 	for (var i = 0; i < 4; i++){
 		corners[i].style.setProperty("-webkit-transition", "width 0s, height 0s, margin 0s");
+		corners[i].style.width=CORNER_SIZE + "px";
+		corners[i].style.height=CORNER_SIZE + "px";
+		corners[i].style.margin= -CORNER_SIZE/2 + "px";
 	}
 }
 
-function onTouchStart(e){
-//	e.preventDefault();
-	lastX = e.targetTouches[0].pageX;
-	lastY = e.targetTouches[0].pageY;
+function clicked() {
+	this.innerHTML = "CLICKED";
+}
+
+function touchEnter() {
+	this.innerHTML = "entered";
+}
+
+function touchLeave() {
+	this.innerHTML = "left";
+}
+
+
+function onStart(x, y) {
+	lastX = x;
+	lastY = y;
 	startX = lastX;
 	startY = lastY;
-	startDate = new Date();
+	startTime = new Date().getTime();
 	pathL = 0;
 	isDown = true;
 	debug.style.background="blue";
@@ -61,11 +104,10 @@ function onTouchStart(e){
 	}
 }
 
-function onTouchMove(e){
-	e.preventDefault();
-	pathL += Math.sqrt(Math.pow(lastX -e.targetTouches[0].pageX, 2) + Math.pow(lastY-e.targetTouches[0].pageY, 2));
-	lastX = e.targetTouches[0].pageX;
-	lastY = e.targetTouches[0].pageY;
+function onMove(x, y) {
+	pathL += Math.sqrt(Math.pow(lastX -x, 2) + Math.pow(lastY-y, 2));
+	lastX = x;
+	lastY = y;
 	debug.innerHTML = "(" + lastX + "/" + width +", " + lastY + "/" + height +")";
 	drawCorner(distance(0,0,lastX,lastY), tl);
 	drawCorner(distance(width,0,lastX,lastY), tr);
@@ -73,15 +115,15 @@ function onTouchMove(e){
 	drawCorner(distance(width,height,lastX,lastY), br);
 }
 
-function onTouchEnd(e){
+
+function onEnd() {
 	isDown = false;
-	debug.innerHTML = "Time = " + (new Date().getTime() - startDate.getTime()) + 
+	debug.innerHTML = "Time = " + timeFrom(startTime) + 
 	"<br />Distance = " + pathL + 
 	"<br />Displacement = " + distance(lastX, lastY, startX, startY) + 
 	"<br />Distance/Displacement = " + pathL/distance(lastX, lastY, startX, startY);
 	
-//	e.preventDefault();
-	if(new Date().getTime()-startDate.getTime() <= max_time && distance(lastX, lastY, startX, startY)*directness > pathL && click(lastX, lastY)){
+	if(timeFrom(startTime)  <= MAX_TIME && distance(lastX, lastY, startX, startY)*DIRECTNESS > pathL && click(lastX, lastY)){
 		debug.style.background = "green";
 	} else {
 		debug.style.background = "red";
@@ -90,44 +132,85 @@ function onTouchEnd(e){
 	for (var i = 0; i < 4; i++){
 		corners[i].style.setProperty("-webkit-transition", "width .6s, height .6s, margin .6s");
 	}
+	
 
 	
 	for (var i = 0; i < 4; i++){
-		corners[i].style.width="200px";
-		corners[i].style.height="200px";
-		corners[i].style.margin="-100px";
+		corners[i].style.width=CORNER_SIZE + "px";
+		corners[i].style.height=CORNER_SIZE + "px";
+		corners[i].style.margin= -CORNER_SIZE/2 + "px";
 	}
 }
 
-function click(x, y){
-	if (distance(x,y, 0, 0) < 125){
+// appends the name of the corner that was swiped to the debug.innerHTML
+// takes in ending x and y coordinates of pointer
+// returns true if the touch sequence ended on a corner
+function click(x, y) {
+	var c;
+	if (distance(x,y, 0, 0) < 136){
 		c = "tl";
-	} else if (distance(x,y, width, 0) < 125){
+	} else if (distance(x,y, width, 0) < 136){
 		c = "tr";
-	} else if (distance(x,y, 0, height) < 125){
+	} else if (distance(x,y, 0, height) < 136){
 		c = "bl";
-	} else if (distance(x,y, width, height) < 125){
+	} else if (distance(x,y, width, height) < 136){
 		c = "br";
 	} else {
 		c = "no"
 	}
 	debug.innerHTML = debug.innerHTML + "<br />" + c;
-	if (c != "no"){
-		return true;
-	}
-	return false;	
+	return c != "no"
 }
 
-function drawCorner(distance, corner){
-	var size = Math.max((((sensitivity+corner_size)/2-distance)/sensitivity)*200+200, 200);
+// takes in a corner element, and the distance of the pointer from that element
+// scales the corner based off the "magic" equation
+function drawCorner(distance, corner) {
+	// TODO: make this equation make sense...
+	var size = Math.max((((2*SENSITIVITY+CORNER_SIZE)/2-distance)/(2*SENSITIVITY))*CORNER_SIZE+CORNER_SIZE, CORNER_SIZE);
 	corner.style.width = corner.style.height = size + "px";
 	corner.style.margin = -size/2 +"px";
 }
 
-function distance(x1, y1, x2, y2){
+// returns the distance in pixels from point1 to point2
+// takes in the x+y location of both points
+function distance(x1, y1, x2, y2) {
 	return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
 }
 
-function onClick(){
-	debug.style.background="orange";
+// returns the time elapsed since the 'begin' object was created
+// 'begin' object should be created from new Date().getTime();
+function timeFrom(begin) {
+	return new Date().getTime() - begin;
+}
+
+function onTouchStart(e) {
+//	e.preventDefault();
+	onStart(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+	
+}
+
+function onTouchMove(e) {
+	if (e.targetTouches.length == 1){
+		e.preventDefault();
+		onMove(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+	}
+}
+
+function onTouchEnd(e) {
+//	e.preventDefault();
+	onEnd();
+}
+
+function onMouseDown(e) {
+	onStart(e.pageX, e.pageY);
+}
+
+function onMouseMove(e) {
+	if (isDown){
+		onMove(e.pageX, e.pageY);
+	}
+}
+
+function onMouseUp(e) {
+	onEnd();
 }
