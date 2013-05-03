c1c14a81f59d2c094049dd271a0a56fcc528d62d
Tore Hanssen <tk.hanssen@gmail.com>
2013-05-03 11:20:10 -0700

    Climate only

--- app.js
+++ app.js
@@ -17,10 +17,19 @@ Ext.application({
         // Destroy the #appLoadingIndicator element
         Ext.fly('appLoadingIndicator').destroy();
 
-        Ext.Viewport.add([Ext.create('feel-your-way.view.Main', {
-            id: 'appContainer',
+//        
+//        var climateApp = Ext.create('feel-your-way.view.ClimateMain', {
+//            id: 'pageContainer',
+//            fullscreen: true,
+//        });
+//        Ext.Viewport.add([climateApp]); // TODO dont add audio if music is playing!
+//        var mainView = Ext.ComponentQuery.query('#appContainer')[0];
+//        Ext.Viewport.remove(mainView, true);
+//        
+        Ext.Viewport.add([Ext.create('feel-your-way.view.ClimateMain', {
+            id: 'pageContainer',
             fullscreen: true,
-        })]);
+        });]);
 
     }
 });
