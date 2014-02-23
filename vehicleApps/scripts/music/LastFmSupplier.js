efd4c538c65061e0adaed790dcfc5bbd0e6d6280
Nicholas Clawson <nickclaw@gmail.com>
2014-02-22 16:26:53 -0800

    Refactored LastFmSupplier

--- vehicleApps/scripts/music/LastFmSupplier.js
+++ vehicleApps/scripts/music/LastFmSupplier.js
@@ -1,8 +1,72 @@
 (function() {
 	var key = process.env.LAST_FM_KEY,
 		secret = process.env.LAST_FM_SECRET;
+	
+	/**
+	 * SupplierClass
+	 * 
+	 * Optional username and password providing they're set in the
+	 * environment
+	 *
+	 * TODO fetched playlists have a set duration for when they can be used,
+	 *		keep track of that and refresh playlist when needed
+	 */
+	var supplier = window.LastFmSupplier = function(callback, username, password) {
+		this.token = null,
+		this.queue = [],
+		this._username = username || process.env.LAST_FM_USERNAME,
+		this._password = password || process.env.LAST_FM_PASSWORD;
 
-	function makeRequest(params) {
+		this._init(callback);
+	};
+
+	/**
+	 * Initialize the object by getting authentication,
+	 * tuning into recomended radio, and getting tracks
+	 */
+	supplier.prototype._init = function(callback) {
+		var self = this;
+
+		// when the supplier is first created... TODO init() ?
+		// authenticate user, tune into playlist, get tracks
+		$.ajax({
+			url : 'https://ws.audioscrobbler.com/2.0/',
+			method : 'POST',
+			data : self._makeRequest({
+				'method' : 'auth.getMobileSession',
+				'username' : self._username,
+				'password' : self._password,
+				'api_key' : key
+			}),
+			success : function(data) {
+				self.token = data.querySelector('key').textContent;
+
+				// prepare radio shit
+				$.ajax({
+					url : 'http://ws.audioscrobbler.com/2.0/',
+					method : 'POST',
+					data : self._makeRequest({
+						method : 'radio.tune',
+						api_key : key,
+						sk : self.token,
+						station : 'lastfm://user/' + self._username + '/recommended'
+					}),
+					success : function(data) {
+						self._getTracks(function(data) {
+							self.queue = data;
+							callback();
+						});				
+					}
+				});
+			}
+		});
+	};
+
+	/**
+	 * Creates an api_sig based off your parameters as specified
+	 * in section 4 of http://www.last.fm/api/mobileauth
+	 */
+	supplier.prototype._makeRequest = function(params) {
 		var unhashed = Object.keys(params)
 			.filter(function(value) {
 				return params.hasOwnProperty(value);
@@ -14,20 +78,24 @@
 				return prev + curr + params[curr];
 			}, '') + secret;
 
-		console.log(unhashed);
-
 		params.api_sig = require('crypto').createHash('md5', 'utf8').update(unhashed).digest('hex');
 		return params;	
-	}
+	};
 
-	function getTracks(token, callback) {
+	/**
+	 * Fetches recent tracks from most current playlist
+	 * http://www.last.fm/api/radio
+	 */
+	supplier.prototype._getTracks = function(callback) {
+		var self = this;
+
 		$.ajax({
 			url : 'http://ws.audioscrobbler.com/2.0/',
 			method : 'POST',
-			data : makeRequest({
+			data : self._makeRequest({
 				method : 'radio.getPlaylist',
 				api_key : key,
-				sk : token,
+				sk : self.token,
 				discovery : true
 			}),
 			success : function(data) {
@@ -49,71 +117,36 @@
 				callback(jsonData);
 			}
 		});
-	}
+	};
 
-	window.LastFmSupplier = function(username, password, callback) {
-		var token = null,
-			queue = [];
 
+	/**
+	 * Going back is not allowed for radio
+	 */
+	supplier.prototype.previous = function() {
+		return null;
+	};
+
+	/**
+	 * Get the next element, refill the queue if needed
+	 */
+	supplier.prototype.next = function() {
 		var self = this;
 
-		/**
-		 * Going back is not allowed for radio
-		 */
-		this.previous = function() {
-			return null;
-		};
-
-		/**
-		 * Get the next element, refill the queue if needed
-		 */
-		this.next = function() {
-			var next = queue.shift();
-			if (queue.length === 0) {
-				getTracks(token, function(data) {
-					queue = data;
-				});
-			}
-			return next;
-		};
-
-		// this radio never ends, i think
-		this.isDone = function() {
-			return false;
-		};
-
-		// when the supplier is first created... TODO init() ?
-		// authenticate user, tune into playlist, get tracks
-		$.ajax({
-			url : 'https://ws.audioscrobbler.com/2.0/',
-			method : 'POST',
-			data : makeRequest({
-				'method' : 'auth.getMobileSession',
-				'username' : username,
-				'password' : password,
-				'api_key' : key
-			}),
-			success : function(data) {
-				token = data.querySelector('key').textContent;
-
-				// prepare radio shit
-				$.ajax({
-					url : 'http://ws.audioscrobbler.com/2.0/',
-					method : 'POST',
-					data : makeRequest({
-						method : 'radio.tune',
-						api_key : key,
-						sk : token,
-						station : 'lastfm://user/' + username + '/recommended'
-					}),
-					success : function(data) {
-						getTracks(token, function(data) {
-							queue = data;
-							callback(self);
-						});				
-					}
-				});
-			}
-		});
+		var next = self.queue.shift();
+		if (self.queue.length === 0) {
+			self._getTracks(function(data) {
+				self.queue = data;
+			});
+		}
+		return next;
 	};
+
+	/**
+	 *  this radio never ends, i think
+	 */ 
+	supplier.prototype.isDone = function() {
+		return false;
+	};
+
 })();
