bc7cf5316bf56976c1f14674dc039eb2cf54e452
Nicholas Clawson <nickclaw@gmail.com>
2014-02-22 16:49:19 -0800

    Comment Music.js

--- vehicleApps/scripts/music/Music.js
+++ vehicleApps/scripts/music/Music.js
@@ -4,6 +4,10 @@
 		currentlyPlaying = null,
 		supplier = null;
 
+	/**
+	 * Takes in a song object or source string a plays it
+	 * Attempts to play the next song after the song ends
+	 */
 	function play(song) {
 		if (currentlyPlaying) { 
 			music.pause();
@@ -32,7 +36,7 @@
 	}
 
 	/**
-	 * Fire and forget type playing 
+	 * Fire and forget
 	 * Useful for notifications or sounds that need to be layered over other sounds
 	 */
 	music.playOver = function(url, volume) {
@@ -46,10 +50,17 @@
 		audio.src = url;
 	};
 
+	/**
+	 * Sets the supplier
+	 */
 	music.setSupplier = function(newSupplier) {
 		supplier = newSupplier;
 	};
 
+	/**
+	 * Starts playing
+	 * Throws an error if supplier isn't set
+	 */
 	music.start = function() {
 		if (supplier) {
 			this.next();
@@ -58,6 +69,10 @@
 		}
 	};
 
+	/**
+	 * Stops the music and unsets the supplier
+	 * Throws error if supplier isn't set
+	 */
 	music.stop = function() {
 		if (supplier) {
 			this.pause();
@@ -68,6 +83,10 @@
 		}
 	};
 
+	/**
+	 * Plays music if paused and there is one
+	 * Throws error if supplier isn't set
+	 */
 	music.play = function() {
 		if (supplier) {
 			if (currentlyPlaying) {
@@ -78,6 +97,10 @@
 		}
 	};
 
+	/**
+	 * Pauses the music
+	 * Throws error if supplier isn't set
+	 */
 	music.pause = function() {
 		if (supplier) {
 			if (currentlyPlaying) {
@@ -88,16 +111,22 @@
 		}
 	};
 
+	/**
+	 * Attemps to the play the next song
+	 * Throws error if supplier isn't set
+	 */
 	music.next = function(autoplay) {
 		if (supplier) {
-			if (!supplier.isDone()) {
-				play(supplier.next());
-			}
+			play(supplier.next());
 		} else {
 			throw "Supplier must be set.";
 		}
 	};
 
+	/**
+	 * Attempts to play the last song
+	 * Throws error if supplier isn't set
+	 */
 	music.previous = function(autoplay) {
 		if (supplier) {
 			if (currentlyPlaying && currentlyPlaying.element.currentTime > 3) {
@@ -111,12 +140,20 @@
 		}
 	};
 
+	/*
+	 * Starts song at given time
+	 * Untested... TODO
+	 */
 	music.seek = function(seconds) {
 		if (currentlyPlaying) {
 			currentlyPlaying.element.currentTime = Math.max(Math.min(seconds, currentlyPlaying.element.duration), 0);
 		}
 	};
 
+	/** 
+	 * Returns info about currently playing song
+	 * Empty object if nothing is playing or set to play
+	 */
 	music.getInfo = function() {
 		return !currentlyPlaying ? {} : {
 			src : currentlyPlaying.src,
